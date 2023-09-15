ed96df6aae36fe5c1253cc6442d71470977d5952
dawitalx <dawitvid@gmail.com>
2023-09-15 07:51:20 -0700

    code updated

---
+++ printf.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf - print all with the formmated output
+ * @format: the format
+ * @...: the args
+ * Return: 0 or NULL
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	char chr;
+	char *str;
+	int count = 0;
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				chr = va_arg(args, int);
+				_putchar(chr);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char*);
+				while (*str)
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				format--;
+				_putchar(*format);
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	_putchar('\0');
+
+	va_end(args);
+
+	return (count);
+}
