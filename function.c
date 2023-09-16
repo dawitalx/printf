a247282faf24ee24ac0cdd98d487344cd58f4ed6
Khayaeljuniormokoena <khayamokoena82@gmail.com>
2023-09-16 00:25:49 -0700

    update

--- function.c
+++ function.c
@@ -1,6 +1,41 @@
-#include <unistd.h>
+#include "main.h"
 
-int _putchar(char c)
+void custom_print(int value) /* take s as input to the console */
+/* @param str The string to be printed. */
 {
-	return (write(1, &c, 1));
+char buffer[20]; /* A buffer to store the converted string */
+int i = 0;
+
+/* Handle negative values */
+if (value < 0)
+{
+putchar('-');
+value = -value;
+}
+
+/* Convert the integer to a string*/
+if (value == 0)
+{
+buffer[i++] = '0';
+}
+else
+{
+while (value > 0)
+{
+buffer[i++] = '0' + (value % 10);
+value /= 10;
+}
+}
+
+/* Print the string in reverse order */
+while (i > 0)
+{
+putchar(buffer[--i]);
+}
+}
+int main(void)
+{
+int num = 42;
+custom_print(num); /* Use custom_print to print the integer */
+return (0);
 }
