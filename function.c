0626ca0411621e622d65c1ae1b39055ca9faee69
Khayaeljuniormokoena <khayamokoena82@gmail.com>
2023-09-18 12:36:41 -0700

    update

--- function.c
+++ function.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
+/**
+ * _putchar  - The char to be written.
+ *
+ * @c: The character to be processed.
+ *  This function takes a character "c" and writes it to the standard output
+ * Return: 1 if succesful else -1 on error
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * *print_number - The int to be printed.
+ *
+ * @num: int
+ * This function takes an integer as a parameter and prints it
+ * Return: if num < 0 result '-', else 0
+ */
 char *print_number(int num)
 {
 	static char numi[1024];
